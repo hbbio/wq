c79181c40c69b4d2c4838b6269a430b8a1060fc5
Henri <niceisnice@gmail.com>
2017-05-01 16:37:02 +0200

    import original code

---
+++ wq.go
@@ -0,0 +1,99 @@
+// (c) Henri Binsztok, 2015
+// All Rights Reserved
+
+package wq
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"database/sql"
+)
+
+var waitBeforeStart = true
+
+func Min(x, y int) int {
+    if x < y {
+        return x
+    }
+    return y
+}
+
+func printJob(nb int) {
+  plural := ""
+  if nb > 1 { plural = "s" }
+  fmt.Print("running ", nb, " job", plural) // , list)
+  for i := 1; i <= 3; i++ {
+    if waitBeforeStart { time.Sleep(1000 * time.Millisecond) }
+    fmt.Print(".")
+  }
+  fmt.Println("")
+}
+
+// payload do not have results
+type payload func(*sql.DB, string) error
+
+func worker(db *sql.DB, id int, fn payload, jobs <-chan string, done chan<- bool) {
+  var err error
+  for job := range jobs {
+    fmt.Println("worker", id, ":", job)
+    err = fn(db, job)
+    if err != nil {
+      log.Println(os.Stderr, err)
+    }
+    done <- err == nil
+  }
+}
+
+func Queue(db *sql.DB, fn payload, nbWorkers int, list []string) (int) {
+  // compute and check number of jobs > 0
+  nbJobs := len(list)
+  if nbJobs == 0 || nbWorkers <= 0 {
+    fmt.Println("nothing to do, quitting")
+    return 0
+  }
+  printJob(nbJobs)
+
+  // compute pool size
+  poolSize := Min(nbJobs, nbWorkers)
+
+  // make channels
+  jobs := make(chan string, poolSize)
+  done := make(chan bool, poolSize)
+  var result bool
+  correct := 0
+
+  // 1. create pool of workers
+  for w := 1; w <= poolSize; w++ {
+    go worker(db, w, fn, jobs, done)
+  }
+
+  // 2. iterate, feed initial pool
+  i := 0
+  for i < poolSize {
+    jobs <- list[i]
+    i++
+  }
+
+  // 3. iterate, feed pool when a job is finished
+  for i < nbJobs {
+    result = <- done // wait for one job to finish
+    if result { correct++ }
+    jobs <- list[i] // launch a new job
+    i++
+  }
+  close(jobs)
+
+  // 4. wait for remaining workers to finish
+  i = 0
+  for i < poolSize {
+    result = <- done
+    if result { correct++ }
+    i++
+  }
+  close(done) // necessary?
+
+  fmt.Println("done:", correct, "/", nbJobs, "successful")
+  return correct
+}
---
+++ wq_test.go
@@ -0,0 +1,51 @@
+// (c) Henri Binsztok, 2015
+// All Rights Reserved
+
+package wq
+
+import (
+	"testing"
+	"database/sql"
+)
+
+var dummy_db *sql.DB
+
+func TestMin(t *testing.T) {
+	var x int
+	x = Min(-1, 1)
+	if x != -1 {
+		t.Error("min1")
+	}
+}
+
+func dummy_fn(db *sql.DB, s string) error {
+	return nil
+}
+
+type testT struct {
+	fn payload
+	nbWorkers int
+	list []string
+}
+
+var tests = []testT {
+	{ dummy_fn, 1, []string{} },
+	{ dummy_fn, 1, []string{"foo"} },
+	{ dummy_fn, 1, []string{"foo", "bar"} },
+	{ dummy_fn, 3, []string{} },
+	{ dummy_fn, 3, []string{"foo"} },
+	{ dummy_fn, 3, []string{"foo", "bar"} },
+	{ dummy_fn, 3, []string{"foo", "bar", "foo", "bar"} },
+	{ dummy_fn, -1, []string{} },
+	{ dummy_fn, -1, []string{"foo"} },
+}
+
+func TestQueue(t *testing.T) {
+	waitBeforeStart = false
+	for i, v := range tests {
+		res := Queue(dummy_db, v.fn, v.nbWorkers, v.list)
+		if res != len(v.list) && !(v.nbWorkers <= 0 && res == 0) {
+			t.Error("queue", i)
+		}
+	}
+}
